78f0332f3d44511a2395b1849fcdf6370937ae19
Maria <nikmaria15@gmail.com>
2023-01-17 14:38:32 +0500

    app.js fixed

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ug2i0kdvqluqhg6h?svg=true)](https://ci.appveyor.com/project/dieweltverbrennt/ajs-arraybuffer)
--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ export function getBuffer() {
   return ((input) => {
     const buffer = new ArrayBuffer(data.length * 2);
     const bufferView = new Uint16Array(buffer);
-    for (let i = 0; i < input.length; i += i) {
+    for (let i = 0; i < input.length; i += 1) {
       bufferView[i] = input.charCodeAt(i);
     }
     return buffer;
